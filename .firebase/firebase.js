2c0b8c7732ae8d610543fae9cef6b963c9cdfdc4
kouheidesu <gyarmex@gmail.com>
2021-08-08 09:29:13 +0900

    20210808!

--- .firebase/firebase.js
+++ .firebase/firebase.js
@@ -3,13 +3,13 @@ import firebase from 'firebase'
 if (!firebase.apps.length) {
 	firebase.initializeApp(
 		{
-			apiKey: "sampleDnGtkVa-frvtI3r7SOaFc7PKxAoANKPxA",
-			authDomain: "sample-nuxt-firebase-introduction.firebaseapp.com",
-			projectId: "sample-nuxt-firebase-introduction",
-			storageBucket: "sample-nuxt-firebase-introduction.appspot.com",
-			messagingSenderId: "sample-779950847276",
-			appId: "sample-1:750847276:web:dbf3b5a8bc3c4d641a6889",
-			measurementId: "sample-EKJ1X2XH3"
+			apiKey: "AIzaSyA4OpdafL5Pvmt9Hh16EuyhK1s100iRgFw",
+			authDomain: "fir-pj-c486f.firebaseapp.com",
+			projectId: "fir-pj-c486f",
+			storageBucket: "fir-pj-c486f.appspot.com",
+			messagingSenderId: "920843347854",
+			appId: "1:920843347854:web:5b0245f70ff9f1e351e6df",
+			measurementId: "G-6XZ024SGTS"
 
 		}
 	)
---
+++ pages/logout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="register">
+    <button @click="logout">ログアウト</button>
+    <br />
+    <NuxtLnk to="/">戻る</NuxtLnk>
+  </div>
+</template>
+
+<script>
+import firebase from "~/plugins/firebase";
+export default {
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          alert("ログアウトが完了しました");
+          this.$router.replace("/");
+        });
+    },
+  },
+};
+</script>
